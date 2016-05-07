834b29aacd6ad9ba59d9c4ec0ec24b0b729ccb12
Geoff Bourne <itzgeoff@gmail.com>
2016-05-07 16:56:51 -0500

    Finalize mvn release

--- handle-mvn-release.sh
+++ handle-mvn-release.sh
@@ -4,10 +4,17 @@ OUR_DIR=$(dirname $_)
 . $OUR_DIR/build-common.sh
 
 if [ -z MVN_RELEASE_VER ]; then
+
     check_var MVN_RELEASE_TAG
     check_var MVN_RELEASE_DEV_VER
+
+    set -ex
 
     mvn -B -Dtag=${MVN_RELEASE_TAG} release:prepare \
                  -DreleaseVersion=${MVN_RELEASE_VER} \
                  -DdevelopmentVersion=${MVN_RELEASE_DEV_VER}
+
+    mvn -s settings.xml release:perform
+
+    mvn release:clean
 fi
