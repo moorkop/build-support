387e237f647191bc2cc5728876b99de85247c8cc
Geoff Bourne <itzgeoff@gmail.com>
2016-05-07 16:49:25 -0500

    Add build param driven mvn release script

---
+++ handle-mvn-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUR_DIR=$(dirname $_)
+. $OUR_DIR/build-common.sh
+
+if [ -z MVN_RELEASE_VER ]; then
+    check_var MVN_RELEASE_TAG
+    check_var MVN_RELEASE_DEV_VER
+
+    mvn -B -Dtag=${MVN_RELEASE_TAG} release:prepare \
+                 -DreleaseVersion=${MVN_RELEASE_VER} \
+                 -DdevelopmentVersion=${MVN_RELEASE_DEV_VER}
+fi
