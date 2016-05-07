bf74ec192689ec040e3afed73ea9459f0bb5380b
Geoff Bourne <itzgeoff@gmail.com>
2016-05-07 17:22:32 -0500

    ...set git config

--- handle-mvn-release.sh
+++ handle-mvn-release.sh
@@ -7,8 +7,13 @@ if [[ -v MVN_RELEASE_VER ]]; then
 
     check_var MVN_RELEASE_TAG
     check_var MVN_RELEASE_DEV_VER
+    check_var MVN_RELEASE_USER_EMAIL
+    check_var MVN_RELEASE_USER_NAME
 
-    set -ex
+    set -e
+
+    git config user.email "${MVN_RELEASE_USER_EMAIL}"
+    git config user.name "${MVN_RELEASE_USER_NAME}"
 
     mvn -B -Dtag=${MVN_RELEASE_TAG} release:prepare \
                  -DreleaseVersion=${MVN_RELEASE_VER} \
