4282b302265793ff5e48e880fad1d6d4967b6017
Geoff Bourne <itzgeoff@gmail.com>
2016-07-02 19:11:46 -0500

    Translate 'master' branch into 'latest' docker tag

--- push-image.sh
+++ push-image.sh
@@ -3,18 +3,32 @@
 OUR_DIR=$(dirname $_)
 . $OUR_DIR/build-common.sh
 
+while getopts b:t: var ; do
+  case $var in
+   b)
+    branch=$OPTARG ;;
+   t)
+    tag=$OPTARG ;;
+  esac
+done
+
+if [[ -z $tag ]]; then
+  if [[ $branch == "master" ]]; then
+    tag=latest
+  fi
+fi
+
 echo "Pushing image"
 echo "Repo path: ${DOCKER_REPO_PATH}"
 echo "Module: ${OUR_MODULE:=$(basename $PWD)}"
-echo "Tag: ${IMAGE_TAG:=${CIRCLE_BRANCH:-${CIRCLE_TAG}}}"
+echo "Tag: ${tag}"
 
 check_var DOCKER_EMAIL
 check_var DOCKER_REPO_USER
 check_var DOCKER_REPO_PASSWORD
 check_var DOCKER_REPO_PATH
-check_var IMAGE_TAG
 resolve_vars
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_REPO_USER -p $DOCKER_REPO_PASSWORD $DOCKER_REPO_SERVER
-docker tag $OUR_MODULE $DOCKER_REPO_PATH/$OUR_MODULE:$IMAGE_TAG
-docker push $DOCKER_REPO_PATH/$OUR_MODULE:$IMAGE_TAG
+docker tag $OUR_MODULE $DOCKER_REPO_PATH/$OUR_MODULE:$tag
+docker push $DOCKER_REPO_PATH/$OUR_MODULE:$tag
